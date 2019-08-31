33e2b56881571eb0a8d06d0cf0a11ae7e4e57a49
Vince Carollo <vincecarollo@gmail.com>
2019-08-31 15:27:59 -0500

    Add clients table and model tests

---
+++ spec/models/client_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Client, type: :model do
+
+  describe 'validations' do
+    it { should validate_presence_of :username }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :street_address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zip }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :phone_number }
+  end
+
+  describe 'relationships' do
+    # it { should have_many :lists }
+  end
+
+end
