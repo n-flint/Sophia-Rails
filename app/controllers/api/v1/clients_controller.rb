213f528cf6508a51b94bf6f1b0a326d375cd5adc
Noah Flint <noah.flint24@gmail.com>
2019-09-01 14:59:03 -0600

    added create action, and client strong params

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -6,4 +6,15 @@ class Api::V1::ClientsController < ApplicationController
       render json: { message: "Not Found" }, status: 404
     end
   end
+
+  def create
+    new_client = Client.create(client_params)
+    render json: new_client, status: 201
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:username, :name, :email, :phone_number, :street_address, :city, :state, :zip, :needs, :allergies, :medications)
+  end
 end
