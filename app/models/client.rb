4b7f24a7e48056e1fdbcb630fdc575360d4aaf16
Vince Carollo <vincecarollo@gmail.com>
2019-09-01 11:59:54 -0500

    Refactor client model for readability

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,12 @@
 class Client < ApplicationRecord
-  validates_presence_of :username, :name, :street_address, :city, :state, :zip, :phone_number
+  validates_presence_of :username,
+                        :name,
+                        :street_address,
+                        :city,
+                        :state,
+                        :zip,
+                        :phone_number
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
