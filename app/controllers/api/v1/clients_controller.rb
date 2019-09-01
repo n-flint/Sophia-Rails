a3ea093aaf4ce8e961c4640822b51ed4b3068839
Vince Carollo <vincecarollo@gmail.com>
2019-09-01 13:26:04 -0500

    Add check to see if clients exist before rendering client json object in clients controller show

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ClientsController < ApplicationController
   def show
-    render json: Client.find(params[:id])
+    if Client.exists?(params[:id])
+      render json: Client.find(params[:id])
+    else
+      render json: { message: "Not Found" }, status: 404
+    end
   end
 end
