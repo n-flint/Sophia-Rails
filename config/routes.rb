5acc1f8c17c9ae4d92f15af3427c604ca6fd951e
Noah Flint <noah.flint24@gmail.com>
2019-09-01 14:58:36 -0600

    added create method to clients routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   namespace :api do
     namespace :v1 do
-      resources :clients, only: [:show]
+      resources :clients, only: [:show, :create]
     end
   end
 end
