74e7f107410dd245db2fade3f8422bb9b35872b5
Noah Flint <noah.flint24@gmail.com>
2019-09-01 14:58:14 -0600

    added expectaitons for exact data in the response

--- spec/requests/api/v1/client_creation_spec.rb
+++ spec/requests/api/v1/client_creation_spec.rb
@@ -12,18 +12,26 @@ RSpec.describe 'Clients API' do
                 "email": "example@email.com",
                 "phone_number": "246342176",
                 "needs": "Grocery, Bills",
-                "alergies": "Pollen, Hard-Work",
+                "allergies": "Pollen, Hard-Work",
                 "medications": "Cannabis"
     }.to_json
 
     headers = { 'CONTENT_TYPE' => 'application/json'}
     post '/api/v1/clients', params: new_user, headers: headers
 
-    data = JSON.parse(response.body)
+    data = JSON.parse(response.body, symbolize_names: true)
 
     expect(response.code).to eq('201')
-
-    require 'pry'; binding.pry
-
+    expect(data[:username]).to eq('username1')
+    expect(data[:name]).to eq('name1')
+    expect(data[:street_address]).to eq('123 Fake St.')
+    expect(data[:city]).to eq('Denver')
+    expect(data[:state]).to eq('CO')
+    expect(data[:zip]).to eq('80203')
+    expect(data[:email]).to eq('example@email.com')
+    expect(data[:phone_number]).to eq('246342176')
+    expect(data[:needs]).to eq('Grocery, Bills')
+    expect(data[:allergies]).to eq('Pollen, Hard-Work')
+    expect(data[:medications]).to eq('Cannabis')
   end
 end
