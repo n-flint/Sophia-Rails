dd955f9c676082b5f14b4c2b36a63eda17f0d3f8
Noah Flint <noah.flint24@gmail.com>
2019-09-02 11:53:36 -0600

    added conditionial to create action for unique email and username

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -8,8 +8,13 @@ class Api::V1::ClientsController < ApplicationController
   end
 
   def create
-    new_client = Client.create(client_params)
-    render json: new_client, status: 201
+    if Client.exists?(:username => client_params['username'])
+        render json: { message: "Username Must Be Unique" }, status: 404
+    elsif Client.exists?(:email => client_params['email'])
+          render json: { message: "Email Must Be Unique" }, status: 404
+    else new_client = Client.create(client_params)
+          render json: new_client, status: 201
+    end
   end
 
   private
