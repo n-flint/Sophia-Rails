f0ce3d8b4744dc88159a2e43297dc47652093f5d
Noah Flint <noah.flint24@gmail.com>
2019-09-02 14:57:45 -0600

    handrolled a path route for updating a client

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :clients, only: [:show]
+      patch '/clients/:id', to: 'clients#update'
     end
   end
 end
