1083ee4c448d7507ffbb8559d6a56d8aaba3ede1
Noah Flint <noah.flint24@gmail.com>
2019-09-02 16:05:32 -0600

    created a destroy action, with valid I

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -6,4 +6,13 @@ class Api::V1::ClientsController < ApplicationController
       render json: { message: "Not Found" }, status: 404
     end
   end
+
+  def destroy
+    if Client.exists?(params['id'])
+      Client.find(params['id']).destroy
+      render json: {status: 204}
+    else
+      render json: { message: "Invalid ID" }, status: 404
+    end
+  end
 end
