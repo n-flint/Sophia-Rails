7ce706fb7c24e024c226587da408240268e0eab5
Noah Flint <noah.flint24@gmail.com>
2019-09-03 15:56:15 -0600

    created lists controller
    Co-authored-by: Vince Carollo <vincecarollo@gmail.com>

---
+++ app/controllers/api/v1/lists_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::ListsController < ApplicationController
+
+  def create
+    new_list = List.new(list_params)
+    if new_list.save
+      render json: new_list, status: 201
+    else
+      render json: new_list.errors, status: 404
+    end
+  end
+
+  private
+
+  def list_params
+    jeremey = params.require(:list).permit(:name)
+    jeremey['client_id'] = params['client_id']
+    jeremey
+  end
+end
