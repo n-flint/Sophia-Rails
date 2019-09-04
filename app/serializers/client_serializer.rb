e815e5c9424e18f7ee3fca562b50724177e3e447
Vince Carollo <vincecarollo@gmail.com>
2019-09-04 16:09:49 -0600

    Add check for null needs, medications, and allergies to clientSerializer

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -8,9 +8,9 @@ class ClientSerializer
     @zip = data.zip
     @email = data.email
     @phone_number = data.phone_number
-    @needs = data.needs.split(', ')
-    @allergies = data.allergies.split(', ')
-    @medications = data.medications.split(', ')
+    @needs = data.needs.split(', ') if data.needs
+    @allergies = data.allergies.split(', ') if data.allergies
+    @medications = data.medications.split(', ') if data.medications
     @created_at = data.created_at
     @updated_at = data.updated_at
   end
