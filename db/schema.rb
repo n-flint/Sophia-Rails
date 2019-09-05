f3a098feb092544f877bf461e6f279f0608bb5bf
Vince Carollo <vincecarollo@gmail.com>
2019-09-04 22:54:26 -0600

    Add columns to clients table: password and diet_restrictions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_211700) do
+ActiveRecord::Schema.define(version: 2019_09_05_045330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2019_09_03_211700) do
     t.string "medications"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "diet_restrictions"
+    t.string "password_digest"
   end
 
   create_table "lists", force: :cascade do |t|
