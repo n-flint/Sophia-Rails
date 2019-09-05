2b180afcd108cccd192b245b5574df577e7106f1
Noah Flint <noah.flint24@gmail.com>
2019-09-05 09:31:27 -0600

    created tasks controller

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::TasksController < ApplicationController
+
+  def create
+    new_task = Task.new(task_params)
+    if new_task.save
+      render json: new_task, status: 201
+    else
+      render json: new_task.errors, status: 404
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description, :name, :completed, :list_id)
+  end
+end
