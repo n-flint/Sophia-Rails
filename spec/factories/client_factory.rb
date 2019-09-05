738768b1a1a32b780b7c7c925a7272abecc3dd3b
Vince Carollo <vincecarollo@gmail.com>
2019-09-04 23:00:15 -0600

    Add creation of password and diet_restrictions to client factory

--- spec/factories/client_factory.rb
+++ spec/factories/client_factory.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :client do
     sequence :username {|n| "username#{n}"}
+    password { 'password' }
     sequence :name {|n| "User#{n}"}
     street_address { '123 Test St' }
     city { 'Testville' }
@@ -11,5 +12,6 @@ FactoryBot.define do
     needs { 'Grocery, Bills' }
     allergies { 'Pollen, Ibuprofen' }
     medications { 'test_drug1, test_drug2' }
+    diet_restrictions { 'vegetarian, peanut-free' }
   end
 end
