5bcf987ea0fb2b12d98bfba105af50fd736a976e
Vince Carollo <vincecarollo@gmail.com>
2019-09-06 22:21:16 -0600

    Add client login endpoint to get client id with username and password

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   namespace :api do
     namespace :v1 do
+      post '/login', to: 'login#create'
       resources :clients, only: [:show, :update, :create, :destroy] do
         resources :lists, only: [:create, :index, :update, :destroy] do
           resources :tasks, only: [:create, :index, :update, :destroy]
