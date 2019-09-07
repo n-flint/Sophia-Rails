82fefc75e9883aaba0e25bc84c1ecc1454af1598
Vince Carollo <vincecarollo@gmail.com>
2019-09-06 22:39:33 -0600

    Add protect_from_forgery method to login controller to allow requests from outside app

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::LoginController < ApplicationController
+  protect_from_forgery with: :null_session
+  
   def create
     client = Client.find_by(username: params[:username])
     if client && client.authenticate(params[:password])
