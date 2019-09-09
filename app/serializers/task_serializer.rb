e0dd77b7516b7853b32fc749b9fdcd68c969bffd
Vince Carollo <vincecarollo@gmail.com>
2019-09-08 20:56:40 -0600

    Change task serializer to only create due_date ivar if due date exists in data

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -7,6 +7,6 @@ class TaskSerializer
     @completed = data[:completed]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
-    @due_date = data[:due_date].strftime("%m/%d")
+    @due_date = data[:due_date].strftime("%m/%d") if data[:due_date]
   end
 end
