267dc74b727068beee1fb56c39ca6427c7ba9b6a
Vince Carollo <vincecarollo@gmail.com>
2019-09-10 16:27:02 -0600

    Change google service to use different sample rate hertz if being tested

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -2,10 +2,16 @@ class GoogleService
   def speech_to_text(audio_path)
     speech = Google::Cloud::Speech.new
 
+    if ENV['RAILS_ENV'] = 'test'
+      sample_rate_hertz = 44100
+    else
+      sample_rate_hertz = 22050
+    end
+
     audio_file = File.binread audio_path
     config     = { encoding: :FLAC,
                    audio_channel_count: 2,
-                   sample_rate_hertz: 22050,
+                   sample_rate_hertz: sample_rate_hertz,
                    language_code: "en-US" }
     audio      = { content: audio_file }
 
