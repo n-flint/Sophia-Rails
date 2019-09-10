c1c896cdb5e9ac3b3be71e9ffa7d44cc51ead112
Noah Flint <noah.flint24@gmail.com>
2019-09-10 17:30:17 -0600

    added sad path testing for invalid ID, non-unique username or email

--- spec/requests/api/v1/caretakers/caretaker_update_spec.rb
+++ spec/requests/api/v1/caretakers/caretaker_update_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Caretakers API' do
-  it 'can update a caretakers profile' do
-    updated_caretaker = {
+  before :each do
+    @updated_caretaker = {
       'username': 'updated_caretaker_1',
       'name': 'updated_caretaker_uno', 
       'email': 'updated_kate@email.com',
@@ -10,7 +10,7 @@ RSpec.describe 'Caretakers API' do
       'abilities': 'updated_ability_1'
     }
     
-    caretaker_1 = Caretaker.create({
+    @caretaker_1 = Caretaker.create({
       'username': 'caretaker_1',
       'password': 'password',
       'password_confirmation': 'password',
@@ -20,7 +20,7 @@ RSpec.describe 'Caretakers API' do
       'abilities': 'ability_1'
     })
 
-    caretaker_2 = Caretaker.create({
+    @caretaker_2 = Caretaker.create({
       'username': 'caretaker_2',
       'password': 'password',
       'password_confirmation': 'password',
@@ -30,9 +30,13 @@ RSpec.describe 'Caretakers API' do
       'abilities': 'ability_1'
     })
 
+
+
     @headers = { 'CONTENT_TYPE' => 'application/json'}
 
-    patch "/api/v1/caretakers/#{caretaker_1.id}", params: updated_caretaker.to_json, headers: @headers
+  end
+  it 'can update a caretakers profile' do
+    patch "/api/v1/caretakers/#{@caretaker_1.id}", params: @updated_caretaker.to_json, headers: @headers
 
     data = JSON.parse(response.body)
 
@@ -41,5 +45,40 @@ RSpec.describe 'Caretakers API' do
     expect(data['email']).to eq('updated_kate@email.com')
     expect(data['phone_number']).to eq('updated_1234567891')
     expect(data['abilities']).to eq('updated_ability_1')
+  end
+
+  it 'receives a 404 if the caretaker ID is invalid' do
+    patch "/api/v1/caretakers/100", params: @updated_caretaker.to_json, headers: @headers
+
+    data = JSON.parse(response.body)
+
+    expect(response.code).to eq('404')
+    expect(data['message']).to eq('Invalid ID')
+  end
+
+  it 'receives a 404 if the username provided is not unique' do
+    invalid_caretaker = {
+                "username": "#{@caretaker_2.username}"
+      }.to_json
+
+    patch "/api/v1/caretakers/#{@caretaker_1.id}", params: invalid_caretaker, headers: @headers
+
+    data = JSON.parse(response.body)
+
+    expect(response.code).to eq('404')
+    expect(data['message']).to eq('Username Must Be Unique')
+  end
+
+  it 'receives a 404 if the email provided is not unique' do
+    invalid_caretaker = {
+                "email": "#{@caretaker_2.email}"
+      }.to_json
+
+    patch "/api/v1/caretakers/#{@caretaker_1.id}", params: invalid_caretaker, headers: @headers
+
+    data = JSON.parse(response.body)
+
+    expect(response.code).to eq('404')
+    expect(data['message']).to eq('Email Must Be Unique')
   end
 end
