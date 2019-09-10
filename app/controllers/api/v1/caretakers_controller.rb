521a1124985365739cf39c58d4c0b2f9ec821899
Noah Flint <noah.flint24@gmail.com>
2019-09-10 17:30:38 -0600

    added conditional to update action for caretaker

--- app/controllers/api/v1/caretakers_controller.rb
+++ app/controllers/api/v1/caretakers_controller.rb
@@ -10,9 +10,17 @@ class Api::V1::CaretakersController < ApplicationController
   end
 
   def update
-    caretaker = Caretaker.find(params['id'])
-    caretaker.update(caretaker_params)
-    render json: caretaker, status: 200
+    if Caretaker.exists?(username: caretaker_params['username'])
+      render json: { message: "Username Must Be Unique" }, status: 404
+    elsif Caretaker.exists?(email: caretaker_params['email'])
+      render json: { message: "Email Must Be Unique" }, status: 404
+    elsif Caretaker.exists?(id: params['id'])
+      caretaker = Caretaker.find(params['id'])
+      caretaker.update(caretaker_params)
+      render json: caretaker, status: 200
+    else
+      render json: { message: "Invalid ID" }, status: 404
+    end
   end
 
   private
