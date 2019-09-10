c653d256118378051e8e39b498f79220383c4a12
Noah Flint <noah.flint24@gmail.com>
2019-09-10 10:59:01 -0600

    removed typos, added commment about strong params

--- app/controllers/api/v1/caretakers_controller.rb
+++ app/controllers/api/v1/caretakers_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::CaretakersController < ApplicationController
 
   def create
-    new_caretaker = CareTaker.new(caretaker_params)
+    new_caretaker = Caretaker.new(caretaker_params)
     if new_caretaker.save
       render json: new_caretaker, status: 201
     else
-      render json: new_characters.errors, status: 404
+      render json: new_caretaker.errors, status: 400
     end
   end
 
@@ -13,6 +13,7 @@ class Api::V1::CaretakersController < ApplicationController
   private
 
   def caretaker_params
-    params.require(:caretaker).permit(:username, :password, :password_confirmation, :name, :email, :phone_number, :abilities)
+    # is this still secure when I am not using the require method?
+    params.permit(:username, :password, :password_confirmation, :name, :email, :phone_number, :abilities)
   end
 end
