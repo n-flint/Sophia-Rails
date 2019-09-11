d1dabbf480f5e3e83b61594f6ba06b4943109e6f
Noah Flint <noah.flint24@gmail.com>
2019-09-11 17:24:39 -0600

    added show action to caretaker/lists controller

--- app/controllers/api/v1/caretakers/lists_controller.rb
+++ app/controllers/api/v1/caretakers/lists_controller.rb
@@ -7,4 +7,15 @@ class Api::V1::Caretakers::ListsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render json: { message: "Not Found" }, status: 404
   end
+
+  def show
+    caretaker = Caretaker.find(params[:id])
+    if list = caretaker.lists.find(params[:list_id])
+      render json: list, status: 200
+    else
+      render json: list.errors, status: 404
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { message: "Not Found" }, status: 404
+  end
 end
