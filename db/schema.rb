d11738d6246f78ff16b92f476975d5cd173903ed
Noah Flint <noah.flint24@gmail.com>
2019-09-10 21:04:10 -0600

    added type to clients/caretakers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_155316) do
+ActiveRecord::Schema.define(version: 2019_09_11_021927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_155316) do
     t.string "abilities"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   create_table "clients", force: :cascade do |t|
@@ -42,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_155316) do
     t.datetime "updated_at", null: false
     t.string "diet_restrictions"
     t.string "password_digest"
+    t.string "type"
     t.index ["username"], name: "index_clients_on_username"
   end
 
