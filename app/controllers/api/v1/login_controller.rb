377660edd957fc9127b8e2b921882cf3a6e1ab3c
Vince Carollo <vincecarollo@gmail.com>
2019-09-11 16:03:07 -0600

    Change login controller to accept caretaker logins

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,12 +1,29 @@
 class Api::V1::LoginController < ApplicationController
   protect_from_forgery with: :null_session
-  
+
   def create
-    client = Client.find_by(username: params[:username])
+    client = find_client
+    caretaker = find_caretaker
     if client && client.authenticate(params[:password])
       render json: ClientSerializer.new(client)
+    elsif caretaker && caretaker.authenticate(params[:password])
+      render json: CaretakerSerializer.new(caretaker)
     else
       render json: { message: "Incorrect Username/Password Combination" }, status: 400
     end
+  end
+
+  private
+
+  def find_client
+    Client.find_by(username: params[:username])
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
+
+  def find_caretaker
+    Caretaker.find_by(username: params[:username])
+  rescue ActiveRecord::RecordNotFound
+    nil
   end
 end
