55b48b2a5dc8e9d4a97243970ba582bed4c8613c
Vince Carollo <vincecarollo@gmail.com>
2019-09-11 16:48:20 -0600

    Update tasks_controller.rb

--- app/controllers/api/v1/caretakers/tasks_controller.rb
+++ app/controllers/api/v1/caretakers/tasks_controller.rb
@@ -1,15 +1,6 @@
 class Api::V1::Caretakers::TasksController < ApplicationController
   protect_from_forgery with: :null_session
 
-  # def create
-  #   new_task = Task.new(task_params)
-  #   if new_task.save
-  #     render json: TaskSerializer.new(new_task), status: 201
-  #   else
-  #     render json: new_task.errors, status: 404
-  #   end
-  # end
-
   def index
     # require 'pry'; binding.pry
     caretaker = Caretaker.find(params[:id])
@@ -18,27 +9,7 @@ class Api::V1::Caretakers::TasksController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render json: { message: "Not Found" }, status: 404
   end
-
-  # def update
-  #   client = Client.find(params[:client_id])
-  #   list = client.lists.find(params[:list_id])
-  #   task = list.tasks.find(params[:id])
-  #   task.update_attributes(task_params)
-  #   render json: TaskSerializer.new(task)
-  # rescue ActiveRecord::RecordNotFound
-  #   render json: { message: "Not Found" }, status: 404
-  # end
-
-  # def destroy
-  #   client = Client.find(params[:client_id])
-  #   list = client.lists.find(params[:list_id])
-  #   task = list.tasks.find(params[:id])
-  #   task.destroy
-  #   render json: {}, status: 204
-  # rescue ActiveRecord::RecordNotFound
-  #   render json: { message: "Not Found" }, status: 404
-  # end
-
+  
   private
 
   def task_params
