018e6cde992e4e4a3bd398f100ea84b5f3cff078
Vince Carollo <vincecarollo@gmail.com>
2019-09-24 14:55:24 -0600

    Add new api/v1/lists endpoint and tests for getting lists

---
+++ app/controllers/api/v1/lists_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::ListsController < ApplicationController
+  def index
+    client = find_client
+    caretaker = find_caretaker
+    render_lists(client, caretaker)
+  end
+
+  private
+
+  def render_lists(client, caretaker)
+    if client
+      render json: client.lists
+    elsif caretaker
+      render json: caretaker.lists
+    else
+      render json: { message: 'Not Found' }, status: 404
+    end
+  end
+
+  def find_client
+    client = Client.find(params[:client_id])
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
+
+  def find_caretaker
+    caretaker = Caretaker.find(params[:caretaker_id])
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
+end
