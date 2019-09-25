51834722c9d0f03ea71d2b2542621d1ff3968b90
Vince Carollo <vincecarollo@gmail.com>
2019-09-25 12:59:11 -0600

    Change tasks completed attribute to default to false

--- db/migrate/20190904235439_create_tasks.rb
+++ db/migrate/20190904235439_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :name
       t.text :description
-      t.boolean :completed
+      t.boolean :completed, default: false
       t.references :list, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2019_09_11_021927) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.bigint "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
